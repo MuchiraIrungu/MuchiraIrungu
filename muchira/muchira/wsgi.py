6ec556d362d0a17e47b8d9ade4d98a7afec8c605
MuchiraIrungu <139977632+MuchiraIrungu@users.noreply.github.com>
2025-01-04 20:14:36 +0300

    Update wsgi.py

--- muchira/muchira/wsgi.py
+++ muchira/muchira/wsgi.py
@@ -11,20 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muchira.muchira.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muchira.settings')
 
 application = get_wsgi_application()
 
-import os
-import sys
+app = application 
 
-# Add your project directory to the sys.path
-project_home = '/home/MuchiraIrungu/portfolio'
-if project_home not in sys.path:
-    sys.path.append(project_home)
 
-# Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolio.settings'
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
